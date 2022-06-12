f496ec727a38af44f6b86047c76dc9cb7a3a5013
Aadarsh Mukati <aadarsh11433@gmail.com>
2022-06-12 12:05:19 +0530

    examples added in theory qus.

--- Problem3/Main.java
+++ Problem3/Main.java
@@ -1,10 +1,57 @@
 package com.Problem3;
 
+class Bird{
+	void transport(){
+		
+		System.out.println("mainly by flying");
+	}
+}
+
+class Penguin extends Bird{
+
+	 @Override
+	 void transport() {
+		 System.out.println("mainly by walking and swimming");
+	 }
+ 
+}
+
+class Ostrich extends Bird{
+
+	 @Override
+	 void transport() {
+		 System.out.println("mainly by walking and running");
+	 }
+
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
 }
+  /*
+                <<---------------------------------DYNAMIC POLYMORPHISM------------------------------------->>
+                   
+                  --->>  Method overriding is dynamic polymorphism.
+                  
+                  --->>  method overloading is static polymorphism.(change in method signature)
+                  
+                  --->> DP is  also called runtime polymorphism. which method will be called decide at runtime.
+                  
+                  --->>  with inheritence we achieve it.(class extends to other)
+                  
+                  --->>  used to change the method implemantation if parent class or superclass implementation is not desirable.  
+                   
+                  --->>  In LINE 13 and 22 Penguin and Ostrich class are overriding the transport method accordiing to their 
+                         relative specifications. 
+                  
+   
+
+
+
+*/
--- Problem4/Main.java
+++ Problem4/Main.java
@@ -5,14 +5,24 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		Animal[] aanimalArr = new Animal[3];
+		Animal[] animalArr = new Animal[3];
 		
 		Dog d1  = new Dog();
 		 Cat c1  = new Cat();
 		Tiger t1  = new Tiger();
 		
+		animalArr[0] = d1;
+		animalArr[1] = c1;
+
+		animalArr[2] = t1;
 		
+	    for(Animal a : animalArr) {
+	    	a.eat();
+	    	a.makeNoise();
+	    	a.walk();
+	    }
 		
+
 		
 	}
 
